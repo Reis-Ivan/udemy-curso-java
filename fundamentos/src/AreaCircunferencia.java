80d4bef010f59b0c74dab014f39fb542ecaffab1
Ivan <ivan.reisrosa@gmail.com>
2023-03-29 21:59:17 -0300

    Commit inicial

---
+++ fundamentos/src/AreaCircunferencia.java
@@ -0,0 +1,16 @@
+public class AreaCircunferencia {
+
+    public static void main(String[] args) {
+
+        double raio = 3.4;
+        final double PI = 3.14156;
+
+        double area = PI * raio * raio;
+        System.out.println("Área = " + area + " m2");
+
+        raio = 10;
+        area = PI * raio * raio;
+        System.out.println("Área = " + area + " m2");
+
+    }
+}
