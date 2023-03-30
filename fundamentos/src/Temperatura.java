6baef5b854cd57ec951bdf274ec4f23f048af926
Ivan <ivan.reisrosa@gmail.com>
2023-03-29 23:13:55 -0300

    desafio conversao de temperatura

---
+++ fundamentos/src/Temperatura.java
@@ -0,0 +1,18 @@
+public class Temperatura {
+
+    public static void main(String[] args) {
+        // (ºF - 32) * 5/9 = ºC
+
+        final int C1 = 32;
+        final double C2 = 5.0 / 9.0;
+
+        double temperaturaF = 86;
+        double temperaturaC = (temperaturaF - C1) * C2;
+        System.out.println(temperaturaF + " ºF" + " = " + temperaturaC + " ºC");
+
+        temperaturaF = 150;
+        temperaturaC = (temperaturaF - C1) * C2;
+        System.out.println(temperaturaF + " ºF" + " = " + temperaturaC + " ºC");
+
+    }
+}
