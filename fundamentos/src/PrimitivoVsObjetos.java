b7ac7b806cd01c5f0eb071170b6d73b48c79c0bd
Ivan <ivan.reisrosa@gmail.com>
2023-03-30 09:11:58 -0300

    diferenças entre tipos primitivos e objetos

---
+++ fundamentos/src/PrimitivoVsObjetos.java
@@ -0,0 +1,13 @@
+public class PrimitivoVsObjetos {
+
+    public static void main(String[] args) {
+
+        String s = "texto";
+        s.toUpperCase();
+
+        // Wrappers → versão objeto dos tipos primitívos
+        int a = 123;
+        System.out.println();
+
+    }
+}
